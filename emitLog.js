64975c9156737446fee1541f6945471c97900ba8
Mcdavid Emereuwa <mcdavid@fluidangle.com>
2020-02-12 17:00:53 +0100

    initial commit

---
+++ emitLog.js
@@ -0,0 +1,29 @@
+#!/usr/bin/env node
+
+let amqp = require('amqplib/callback_api');
+
+amqp.connect('amqp://localhost', ((error0, connection) => {
+		if(error0) {
+			throw error0;
+		}
+		connection.createChannel((error1, channel) => {
+			if (error1) throw error1;
+			let exchange = 'logs';
+			let msg = process.argv.slice(2).join(' ') || "Hello World!";
+
+			channel.assertExchange(exchange, 'fanout', {
+				durable: false
+			});
+
+			channel.publish(exchange, '', Buffer.from(msg), {
+        persistent: true
+      });
+      console.log(`[x] Sent ${msg}`);
+      
+      setTimeout(() => {
+        connection.close();
+        process.exit(0);
+      }, 500)
+		});
+	})
+)
